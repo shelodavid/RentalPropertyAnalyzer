c8f0a64ab54e81f390db9a6e192f5a32c86d1af3
shelodavid <sheldon.o.david@gmail.com>
2026-01-22 20:26:42 -0500

    Fix PageHeader PartialAsync tuple null typing

--- RentalPropertyAnalyzer/Models/FilterParameters.cs
+++ RentalPropertyAnalyzer/Models/FilterParameters.cs
@@ -2,11 +2,11 @@
 {
     public class FilterParameters
     {
-        public string PropertyType { get; set; }
-        public string State { get; set; }
-        public string County { get; set; }
+        public string? PropertyType { get; set; }
+        public string? State { get; set; }
+        public string? County { get; set; }
         public int MinPrice { get; set; }
         public int MaxPrice { get; set; }
-        public string SortOrder { get; set; }
+        public string? SortOrder { get; set; }
     }
 }
